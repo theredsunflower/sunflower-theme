f0f02af4d060809a08d1845d8db92b6250b5e69e
Jessica Patterson <jessica@theredsunflower.com>
2024-05-20 15:38:13 -0400

    Update single.php
    Removed recent posts and made semantic changes to HTML

--- single.php
+++ single.php
@@ -1,14 +1,3 @@
-<?php
-/**
- This is the page that is displayed for single posts
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package CodeCamp
- */
-
-get_header(); ?>
-
 <div id="page">
 	<div id="content">
 		<div id="single-content">
@@ -21,8 +10,7 @@ get_header(); ?>
  
     		// the code between the while loop will be repeated for each post
     		?>
-    			<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
-    			<p class="date-author">Posted: <?php the_date(); ?> by <?php the_author(); ?></p>
+    			<h1 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
    				<?php the_content(); ?> 
     		<?php
     		// Stop the loop when all posts are displayed
@@ -35,24 +23,5 @@ get_header(); ?>
 				endif;
 			?>
 		</div>
-		<div id="single-recent-posts">
-			<h1 class="front-page-title">Blog</h1>
-	    		<?php $catquery = new WP_Query( 'cat=1&posts_per_page=3' ); ?>
- 
-				<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
- 				<div class="content-item">
-					<a href="<?php the_permalink() ?>" rel="bookmark">
-						<h2><?php the_title(); ?></h2>
-						<?php the_post_thumbnail(); ?>	
-					</a>
-				</div>
-
-				<?php endwhile;
-    				wp_reset_postdata();
-				?>
-			</div>
 	</div><!--content-->
 </div><!--page-->
-
-<?php
-get_footer(); ?>
