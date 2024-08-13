334bf45475c9f783ec65c554b34493f85dce9dd2
Jessica Patterson <jessica@theredsunflower.com>
2024-08-13 13:39:44 -0400

    Update single.php
    replaced missing header and footer info

--- single.php
+++ single.php
@@ -1,27 +1,41 @@
+<?php
+/**
+ This is the page that is displayed if nothing more specific exists
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package CodeCamp
+ */
+
+get_header(); ?>
+
 <div id="page">
-	<div id="content">
-		<div id="single-content">
-			<?php
-			// checks if there are any posts that match the query
-			if (have_posts()) :
- 
-  			// If there are posts matching the query then start the loop
- 			while ( have_posts() ) : the_post();
- 
-    		// the code between the while loop will be repeated for each post
-    		?>
-    			<h1 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
-   				<?php the_content(); ?> 
-    		<?php
-    		// Stop the loop when all posts are displayed
- 				endwhile;
-			// If no posts were found
-				else :
-			?>
-			<p>Sorry no posts matched your criteria.</p>
-			<?php
-				endif;
-			?>
+	<div id="content">	
+		<div id="other-content">
+				<?php
+				// checks if there are any posts that match the query
+				if (have_posts()) :
+	 
+	  			// If there are posts matching the query then start the loop
+	 			while ( have_posts() ) : the_post();
+	 
+	    		// the code between the while loop will be repeated for each post
+	    		?>
+	    			<h1 id="post-<?php the_ID(); ?>"><?php the_title(); ?></h1>
+	   				<?php the_content(); ?> 
+	    		<?php
+	    		// Stop the loop when all posts are displayed
+	 				endwhile;
+				// If no posts were found
+					else :
+				?>
+				<p>Sorry no posts matched your criteria.</p>
+				<?php
+					endif;
+				?>
+			</div>
 		</div>
-	</div><!--content-->
 </div><!--page-->
+
+<?php
+get_footer(); ?>
